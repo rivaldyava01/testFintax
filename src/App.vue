e8a6677d8ac676a11783ff5833bb99ff2b3e1fdd
aa <rivaldy.avianto01@gmail.com>
2021-08-08 09:18:32 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div id="app">
+     <Navbar></Navbar>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from './components/navbar.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'app',
+ components: {
+      Navbar
+ }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+body {
+ margin: 0;
+ font-family: Arial, Helvetica, sans-serif;
 }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,64 @@
+<template>
+     <div>
+        <div class="topnav" >
+            <a v-for= "list in navbarList" :key="list.id" :href="list.url" >{{list.title}}</a>
+            <button>Login</button>
+              <span class="navbar-text" >
+                    <a role="button" id="loginBtn">
+                        <span class="fa fa-sign-in"></span> Login
+                    </a>
+                </span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data(){
+    return{
+      navbarList : [
+          {
+              id: "homeId",
+              title: "Home",
+              url: "#home"
+          },
+          {
+              id: "taxListId",
+              title: "Tax List",
+              url: "#taxlist"
+          },
+          {
+              id: "aboutUsId",
+              title: "About Us",
+              url: "#aboutus"
+          },
+      ]
+    }
+  },
+  props: {
+
+  }
+}
+</script>
+
+<style scoped>
+.topnav {
+ overflow: hidden;
+ background-color: #333;
+}
+
+.topnav a {
+ float: left;
+ color: #f2f2f2;
+ text-align: center;
+ padding: 14px 16px;
+ text-decoration: none;
+ font-size: 17px;
+}
+
+.topnav a:hover {
+ background-color: #ddd;
+ color: black;
+}
+</style>
